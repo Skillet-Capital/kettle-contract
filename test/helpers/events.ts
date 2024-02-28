e87edede95777a8e903b85d07c2ca8ca5ca9e33b
Andrew Warner <warneracw@gmail.com>
2024-02-28 10:43:24 -0500

    event extraction

--- test/helpers/events.ts
+++ test/helpers/events.ts
@@ -8,6 +8,16 @@ export async function getTimestamp(block: number) {
   return ethers.provider.getBlock(block).then((block) => block!.timestamp);
 }
 
+type LogName = "Borrow" | "BuyInLien" | "BuyInLienWithLoan" | "BuyWithLoan" | "Claim" | "Initialized" | "Payment" | "Refinance" | "Repay" | "SellInLien" | "SellInLienWithLoan";
+
+function extractLog(receipt: ContractTransactionReceipt, logName: LogName) {
+  const KettleInterface = Kettle__factory.createInterface();
+  const { topicHash } = KettleInterface.getEvent(logName);
+
+  const log = receipt!.logs.find((log) => log.topics[0] === topicHash);
+  return KettleInterface.decodeEventLog(logName, log!.data, log!.topics);
+}
+
 interface BorrowLog {
   lien: LienStruct;
   lienId: string | number | bigint;
@@ -44,13 +54,7 @@ export function extractBorrowLog(receipt: ContractTransactionReceipt): BorrowLog
   }
 }
 
-interface PaymentLog {
-  lienId: string | number | bigint;
-  amount: bigint;
-  amountOwed: bigint;
-  paidThrough: bigint;
-}
-export async function extractPaymentLog(receipt: ContractTransactionReceipt): Promise<PaymentLog> {
+export function extractPaymentLog(receipt: ContractTransactionReceipt) {
   const KettleInterface = Kettle__factory.createInterface();
   const { topicHash } = KettleInterface.getEvent("Payment");
 
@@ -104,5 +108,109 @@ export function extractRefinanceLog(receipt: ContractTransactionReceipt) {
     principal: repay.principal,
     amountOwed: repay.amountOwed,
     amount: repay.amount
+  }
+}
+
+export function extractBuyWithLoanLog(receipt: ContractTransactionReceipt) {
+  const log = extractLog(receipt, "BuyWithLoan");
+
+  return {
+    lienId: log.lienId,
+    buyer: log.buyer,
+    seller: log.seller,
+    currency: log.currency,
+    collection: log.collection,
+    tokenId: log.tokenId,
+    size: log.size,
+    amount: log.amount,
+    borrowAmount: log.borrowAmount
+  }
+}
+
+export function extractBuyInLienLog(receipt: ContractTransactionReceipt) {
+  const log = extractLog(receipt, "BuyInLien");
+
+  return {
+    lienId: log.lienId,
+    buyer: log.buyer,
+    seller: log.seller,
+    currency: log.currency,
+    collection: log.collection,
+    tokenId: log.tokenId,
+    size: log.size,
+    askAmount: log.askAmount,
+    amountOwed: log.amountOwed,
+    principal: log.principal,
+    pastInterest: log.pastInterest,
+    pastFee: log.pastFee,
+    currentInterest: log.currentInterest,
+    currentFee: log.currentFee
+  }
+}
+
+export function extractSellInLienLog(receipt: ContractTransactionReceipt) {
+  const log = extractLog(receipt, "SellInLien");
+
+  return {
+    lienId: log.lienId,
+    buyer: log.buyer,
+    seller: log.seller,
+    currency: log.currency,
+    collection: log.collection,
+    tokenId: log.tokenId,
+    size: log.size,
+    bidAmount: log.bidAmount,
+    amountOwed: log.amountOwed,
+    principal: log.principal,
+    pastInterest: log.pastInterest,
+    pastFee: log.pastFee,
+    currentInterest: log.currentInterest,
+    currentFee: log.currentFee
+  }
+}
+
+export function extractBuyInLienWithLoanLog(receipt: ContractTransactionReceipt) {
+  const log = extractLog(receipt, "BuyInLienWithLoan");
+
+  return {
+    oldLienId: log.oldLienId,
+    newLienId: log.newLienId,
+    buyer: log.buyer,
+    seller: log.seller,
+    currency: log.currency,
+    collection: log.collection,
+    tokenId: log.tokenId,
+    size: log.size,
+    askAmount: log.askAmount,
+    borrowAmount: log.borrowAmount,
+    amountOwed: log.amountOwed,
+    principal: log.principal,
+    pastInterest: log.pastInterest,
+    pastFee: log.pastFee,
+    currentInterest: log.currentInterest,
+    currentFee: log.currentFee
+  }
+}
+
+export function extractSellInLienWithLoanLog(receipt: ContractTransactionReceipt) {
+  const log = extractLog(receipt, "SellInLienWithLoan");
+
+  return {
+    oldLienId: log.oldLienId,
+    newLienId: log.newLienId,
+    buyer: log.buyer,
+    seller: log.seller,
+    currency: log.currency,
+    collection: log.collection,
+    tokenId: log.tokenId,
+    size: log.size,
+    bidAmount: log.bidAmount,
+    borrowAmount: log.borrowAmount,
+    amountOwed: log.amountOwed,
+    principal: log.principal,
+    pastInterest: log.pastInterest,
+    pastFee: log.pastFee,
+    currentInterest: log.currentInterest,
+    currentFee: log.currentFee
   }
 }
