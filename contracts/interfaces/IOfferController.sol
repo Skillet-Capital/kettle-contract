b107fcd6e06d90fab3be8edef9f8f910be2854cd
Andrew Warner <warneracw@gmail.com>
2024-03-25 10:41:19 -0400

    enhance offer taken logs

--- contracts/interfaces/IOfferController.sol
+++ contracts/interfaces/IOfferController.sol
@@ -1,23 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import { MarketOffer, LoanOffer, BorrowOffer } from "../Structs.sol";
+
 interface IOfferController {
 
     event LoanOfferTaken(
         uint256 indexed lienId,
         address indexed taker,
-        bytes32 indexed hash
+        bytes32 indexed hash,
+        uint256 amount,
+        address collection,
+        uint256 tokenId,
+        LoanOffer offer
     );
 
     event BorrowOfferTaken(
         uint256 indexed lienId,
         address indexed taker,
-        bytes32 indexed hash
+        bytes32 indexed hash,
+        address collection,
+        uint256 tokenId,
+        BorrowOffer offer
     );
 
     event MarketOfferTaken(
         address indexed taker,
-        bytes32 indexed hash
+        bytes32 indexed hash,
+        address collection,
+        uint256 tokenId,
+        MarketOffer offer
     );
 
     event OfferCancelled(
